ee13b6999bc437d11204a5b1fbc4619ac3994f5c
serhii-harkusha-dnu <sergei.garkusha+dnu@gmail.com>
2023-12-03 22:02:15 +0200

    Виправлення лаб 1

--- Штучний Інтелект/1. Кластеризація/main.py
+++ Штучний Інтелект/1. Кластеризація/main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.cluster import MeanShift, estimate_bandwidth
 from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.cluster import MeanShift , estimate_bandwidth
 
 X = np.loadtxt('data_clustering.txt', delimiter=',')
 
@@ -17,10 +17,31 @@ plt.ylim(y_min, y_max)
 plt.xticks(())
 plt.yticks(())
 
+#meanshift part - центри кластерів
+bandwidth_X = estimate_bandwidth(X , quantile=0.15, n_samples=len(X))
+
+meanshift_model = MeanShift(bandwidth= bandwidth_X, bin_seeding=True)
+meanshift_model.fit(X)
+
+cluster_centers = meanshift_model.cluster_centers_
+print('\nCenters of clusters:\n', cluster_centers)
+
+labels = meanshift_model.labels_
+num_clusters = len(np.unique(labels))
+print("\nNumber of clusters in input data =", num_clusters)
+
+plt.figure()
+markers = 'o*xvsd<'
+for i, marker in zip(range(num_clusters), markers):
+    plt.scatter(X[labels==i, 0], X[labels==i, 1], marker=marker, color='black')
+    cluster_center = cluster_centers[i]
+    plt.plot(cluster_center[0], cluster_center[1], marker='o', markerfacecolor= 'red', markeredgecolor= 'black', markersize= 15)
+
+plt.title('Clusters')
+
 # Ініціалізаця кластеризованих даних діаграмою
 scores = []
 values = np.arange(2, 10)
-
 
 for num_clusters in values:
     # навчання ШІ на моделях
@@ -40,42 +61,20 @@ plt.bar(values, scores, width=0.5, color='black', align='center')
 plt.title('Silhouette score vs number of clusters')
 
 # показ кращих результатів
-num_clusters = np.argmax(scores) + values[0]
+#num_clusters = np.argmax(scores) + values[0]
+lables = meanshift_model.labels_
+num_clusters = len(np.unique(labels))
 print('\nOptimal number of clusters =', num_clusters)
 
 
-#meanshift part - центри кластерів
-bandwidth_X = estimate_bandwidth(X, quantile=0.05, n_samples=len(X))
-
-meanshift_model = MeanShift(bandwidth=bandwidth_X, bin_seeding=True)
-meanshift_model.fit(X)
-
-cluster_centers = meanshift_model.cluster_centers_
-print('\nCenters of clusters:\n', cluster_centers)
-
-labels = meanshift_model.labels_
-num_clusters = len(np.unique(labels))
-print("\nNumber of clusters in input data =", num_clusters)
-
-plt.figure()
-markers = 'o*xvsd'
-for i, marker in zip(range(num_clusters), markers):
-    plt.scatter(X[labels == i, 0], X[labels == i, 1], marker=marker, color='black')
-    cluster_center = cluster_centers[i]
-    plt.plot(cluster_center[0], cluster_center[1], marker='o', markerfacecolor='red', markeredgecolor='black', markersize=15)
-
-plt.title('Clusters')
-
-
-
 #kmeans part - кластеризовані дані з областями кластеризації
-num_clusters = 7
 
-kmeans = KMeans(init='k-means++', n_clusters=num_clusters, n_init=10)
+
+kmeans = KMeans(init= 'k-means++', n_clusters = num_clusters, n_init=10)
 #навчання КMeans кластиризації на моделях
 kmeans.fit(X)
 #Розмір кроку сітки
-step_size = 0.01
+step_size = 0.1
 #Визначення сіток точок для нанесення меж
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
@@ -87,19 +86,18 @@ output = kmeans.predict(np.c_[x_vals.ravel(), y_vals.ravel()])
 output = output.reshape(x_vals.shape)
 plt.figure()
 plt.clf()
-
 plt.imshow(output, interpolation='nearest',
            extent=(x_vals.min(), x_vals.max(),
                y_vals.min(), y_vals.max()),
-           cmap=plt.cm.get_cmap('nipy_spectral',10),
+           cmap=plt.cm.Paired,
            aspect='auto',
            origin='lower')
 # Накладання точок введення
-plt.scatter(X[:, 0], X[:, 1], marker='o', facecolors='none',
+plt.scatter(X[:,0], X[:,1], marker='o', facecolors='none',
         edgecolors='black', s=80)
 # Побудування центрів кластерів
 cluster_centers = kmeans.cluster_centers_
-plt.scatter(cluster_centers[:, 0], cluster_centers[:, 1],
+plt.scatter(cluster_centers[:,0], cluster_centers[:,1],
         marker='o', s=210, linewidths=4, color='black',
         zorder=12, facecolors='black')
 
